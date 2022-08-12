a4ffe4700c30daaa38e50d08e9c988c47f06ff47
Simon Buckle <simon.buckle@gmail.com>
2022-08-12 16:02:18 +0100

    Changed image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -11,7 +11,7 @@ spec:
       value: tcp://127.0.0.1:2375
     tty: true
   - name: dind
-    image: docker:19.03-dind-rootless
+    image: docker:19.03.0-dind
     env:
     - name: DOCKER_TLS_CERTDIR
       value: ""
