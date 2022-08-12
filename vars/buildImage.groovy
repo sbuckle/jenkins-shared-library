f5c2a0417f5a61639a2f682a8c7f3bbdbaaba938
Simon Buckle <simon.buckle@gmail.com>
2022-08-12 13:34:28 +0100

    Added Docker variable

---
+++ vars/buildImage.groovy
@@ -0,0 +1,25 @@
+def DOCKER = '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker
+    env:
+    - name: DOCKER_HOST
+      value: tcp://locahost:2375
+    tty: true
+  - name: dind
+    image: docker:dind
+    env:
+    - name: DOCKER_TLS_CERTDIR
+      value: ""
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: dind-storage
+      mountPath: /var/lib/docker
+  volumes:
+  - name: dind-storage
+    emptyDir: {}
+'''
