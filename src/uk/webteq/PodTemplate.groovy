cba0e2b29456b781a235fe656886fcbbeae35f53
Simon Buckle <simon.buckle@gmail.com>
2022-08-12 10:43:30 +0100

    Intial commit

---
+++ src/uk/webteq/PodTemplate.groovy
@@ -0,0 +1,30 @@
+public class PodTemplate {
+    
+    static String DOCKER_BUILD_TEMPLATE
+        = getDockerPodTemplate()
+
+    static String getDockerPodTemplate() {
+        return '''
+apiVersion: 1.0
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker
+    tty: true
+  - name: dind
+    image: docker:dind
+    env:
+    - name: DOCKER_TLS_CERTDIR
+      value: ""
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: dind-storage
+      mountPath: /var/lib/docker
+  volumes:
+  - name: dind-storage
+    emptyDir: {}
+'''
+    }
+}
